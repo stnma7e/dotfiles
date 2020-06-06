207bb344d85e352319631adde278c90fba1d9622
Sam Delmerico <svdelmerico@gmail.com>
2020-06-05 22:32:06 -0400

    add new emacs init.el

---
+++ emacs/init.el
@@ -0,0 +1,53 @@
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rust-mode ein async-await company projectile flycheck magit nlinum-relative railscasts-reloaded-theme helm evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
+;; Global configs
+(setq scroll-step           1
+      scroll-conservatively 10000)
+(tool-bar-mode -1)
+
+;;
+;; Package configs
+;;
+;; emacs-async
+(async-bytecomp-package-mode 1)
+
+(require 'evil)
+(evil-mode 1)
+
+(require 'helm-config)
+(helm-mode 1)
+
+(use-package nlinum-relative
+    :config
+    (nlinum-relative-setup-evil)
+    (add-hook 'prog-mode-hook 'nlinum-relative-mode))
+
+;; Flycheck: on-the-fly syntax checking
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; themes
+(use-package railscasts-reloaded-theme
+	:ensure t
+	:init
+	(load-theme 'railscasts-reloaded t))
