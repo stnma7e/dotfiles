73981c8bf8fc0c37e23a58db4661d71112e232a6
Sam Delmerico <svdelmerico@gmail.com>
2020-06-06 13:16:49 -0400

    [emacs] add entire emacs.d directory

---
+++ emacs/elpa/helm-20200605.1547/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200605.1547" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "03a8e4f800130b0ad7cea04f87bab7e18a38a3d3" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/elpa/helm-core-20200605.647/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200605.647" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "03a8e4f800130b0ad7cea04f87bab7e18a38a3d3" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
