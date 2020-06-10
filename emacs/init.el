cdda4cc3dffff8c3f43a49ef871215f6f7978510
Sam Delmerico <svdelmerico@gmail.com>
2020-06-10 14:35:32 -0400

    [emacs] change theme settings

--- emacs/init.el
+++ emacs/init.el
@@ -7,9 +7,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(beacon-color "#f2777a")
+ '(custom-enabled-themes (quote (whiteboard)))
+ '(custom-safe-themes
+   (quote
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "dd4045f095288cdd2ff9ba043e1a6def457e51af762aee7ecdafdbba0877e7c3" default)))
+ '(fci-rule-color "#383838")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id)))
  '(package-selected-packages
    (quote
-    (which-key lsp-ui helm-lsp lsp-mode rust-mode ein async-await company projectile flycheck magit nlinum-relative railscasts-reloaded-theme helm evil))))
+    (rustic vterm which-key lsp-ui helm-lsp lsp-mode ein company projectile flycheck magit nlinum-relative railscasts-reloaded-theme helm evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
